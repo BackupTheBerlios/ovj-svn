9f78d6736dddf7ffbe63fa7d27abfc8b90ea0544
kpa <kpa@cbfa3f45-332c-0410-a637-c6bd3c8c102b>
2007-02-19 00:54:13 +0000

    Finally working correctly under Windows and under Linux.

--- branches/dg0yt/lib/OVJ/Browser.pm
+++ branches/dg0yt/lib/OVJ/Browser.pm
@@ -1,17 +1,6 @@
 # $Id$
 #
-# Some portions (C) 2007 Kai Pastor, DG0YT <dg0yt AT darc DOT de>
-#
-# Based on / major work:
-#
-# Script zum Erzeugen der OV Jahresauswertung für
-# OV Peilwettbewerbe
-# Autor:   Matthias Kuehlewein, DL3SDO
-# Version: 0.96
-# Datum:   14.1.2007
-#
-#
-# Copyright (C) 2007  Matthias Kuehlewein, DL3SDO
+# Copyright (C) 2007 Kai Pastor, DG0YT <dg0yt AT darc DOT de>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -28,9 +17,11 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-=head1 OVJ::Inifile;
+=head1 OVJ::Browser
 
-=head2 Synopsis
+=head1 Synopsis
+
+	use OVJ::Browser;
 
 	OVJ::Browser::open("index.html")
 	 or warn "Konnte Browser nicht starten";
@@ -50,30 +41,43 @@ BEGIN {
 	$VERSION = "0.1";
 }
 
+=head1 Methoden
+
+=head2 open(location)
+
+Öffnet die als Argument 'location' übergebene Adresse (Pfad, URL)
+in einem Browser.
+
+=cut
+
 sub open {
 	my @arg = @_
 	 or carp "usage: OVJ::Browser::open('index.htm')", return ;
 	
-	my $pid = fork;
-	if (! $pid)  {
-		# Kindprozess ($pid == 0) oder fork fehlgeschlagen ($pid == undef)
-		my $ret;
-		if ($^O =~ /Win/i) {
-			# Windows
-			unshift @arg, qw(cmd /c start);
-			$ret = { exec @arg };
-		}
-		else {
-			# Linux et al.
+	if ($^O =~ /Win/i) {
+		# Windows
+		unshift @arg, qw(cmd /c start);
+		return 0 == system @arg;
+	}
+	else {
+		# Linux et al.
+		my $pid = fork;
+		if (defined $pid && $pid == 0) {
+			# Kindprozess ($pid == 0)
+			delete $SIG{'__WARN__'}; # Sonst merkwürdige Fehler bei exec
 			foreach (qw/firefox konqueror/) {
-				$ret = { exec $_, @arg }; # Endet bei Erfolg!
+				{ exec $_, @arg }; # Hier Ende bei Erfolg!
 			}
+			exit 1; # Hier Ende bei Fehler
 		}
-		if ($ret) {
-			print STDERR "Konnte Browser nicht starten: $!\n";
-		}
-		exit ($ret == 0) if defined $pid; # Kindprozess bei Fehler beenden
+		return $pid;
 	}
 }
+
+=head1 Autor
+
+Kai Pastor 
+
+=cut
 
 1;
