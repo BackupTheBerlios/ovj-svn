4f4531aa516659a2f8dbf2d7da53a077d47a9897
kpa <kpa@cbfa3f45-332c-0410-a637-c6bd3c8c102b>
2007-02-26 20:45:38 +0000

    Lazy PATH adjustment.

--- branches/dg0yt/pl2exe.bat
+++ branches/dg0yt/pl2exe.bat
@@ -1,3 +1,11 @@
+@echo off
 echo "$Id$"
-@set PATH=%PATH%;U:\Programme\ActivePerl\bin;U:\Programme\ActivePerl\site\bin
+perl -v
+if not errorlevel 1 (
+	echo Verwende Perl aus Standard-PATH
+) else if exist U:\Programme\ActivePerl\bin\perl.exe (
+	set PATH=%PATH%;U:\Programme\ActivePerl\bin;U:\Programme\ActivePerl\site\bin
+) else (
+	echo Kann Perl nicht finden
+)
 pp --gui --lib lib -o OVJ.exe OVJ.pl 
