e983d84e65e3978207bcf5d65ea0b12be0ddbc6f
kpa <kpa@cbfa3f45-332c-0410-a637-c6bd3c8c102b>
2007-03-22 08:33:48 +0000

    Release preparations.

---
+++ tags/0.971/release.sh
@@ -0,0 +1,4 @@
+test -f OVJ.exe || exit 1
+rm -Rf t test.sh runtests.pl pl2exe.bat OVJ.pl lib release.sh
+find . -name .svn -prune -exec rm -Rf "{}" ";"
+pushd ..; zip -r OVJ-0.971-MSWin.zip OVJ-0.971-MSWin; popd
