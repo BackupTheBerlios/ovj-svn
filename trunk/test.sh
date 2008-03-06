b5e95b6f104961e10c5a5aed5475b486ef6a17a3
kpa <kpa@cbfa3f45-332c-0410-a637-c6bd3c8c102b>
2008-03-06 20:10:29 +0000

    test.sh nimmt Testdatei(en) als Argument.

--- trunk/test.sh
+++ trunk/test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+if test -z "$1" ; then
+  DEFAULT_TESTS=t/*.t
+else
+  DEFAULT_TESTS=
+fi
 perl -Ilib -e 'use Test::Harness qw(&runtests $verbose);
-               $verbose=1; runtests @ARGV;' t/*.t
+               $verbose=1; runtests @ARGV;' $* $DEFAULT_TESTS
